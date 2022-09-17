cbfd2e7ae393812681a84c279a84116cb616b904
cpkopp <cameronpkopp@gmail.com>
2022-09-17 12:56:41 -1000

    questions for inquirer prompt and inquirer import setup

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-
-
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions:'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
